8a825335b864eff182886746fd8b48cd98845a29
SUMMIT KAWAKAMI <summit.kawakami@getmykarma.ca>
2025-01-03 03:30:29 -0700

    hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,25 +7,23 @@ import HeroGrid from '@/components/Homepage/HeroGrid/HeroGrid';
 
 export default function Home() {
   return (
-    <main className={styles.totalMain}>
-      <div className={styles.wrapper}>
-        <Navbar />
-        <div className={styles.main}>
-          <HeroBackground />
-          <Stars />
-        </div>
-
-        <section className={styles.homepageText}>
-          <div className={styles.scrollPast}>
-            <p className={styles.subText}>
-              Electrical engineer, DJ, and aspiring space cadet.
-            </p>
-          </div>
-          <div className={styles.aboutMe}>
-            <HeroGrid />
-          </div>
-        </section>
+    <main className={styles.wrapper}>
+      <Navbar />
+      <div className={styles.main}>
+        <HeroBackground />
+        <Stars />
       </div>
+
+      <section className={styles.homepageText}>
+        <div className={styles.scrollPast}>
+          <p className={styles.subText}>
+            Electrical engineer, DJ, and aspiring space cadet.
+          </p>
+        </div>
+        <div className={styles.aboutMe}>
+          <HeroGrid />
+        </div>
+      </section>
     </main>
   );
 }
